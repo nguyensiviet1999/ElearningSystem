79f6a034690db53a7f75c52a34d1609dcc3f6730
nguyensiviet1999 <nguyensiviet1999@gmail.com>
2020-07-20 14:11:40 +0700

    a

--- db/migrate/20200713071842_add_image_to_courses.rb
+++ db/migrate/20200713071842_add_image_to_courses.rb
@@ -1,6 +1,6 @@
 class AddImageToCourses < ActiveRecord::Migration[6.0]
   def change
-    add_column :Courses, :image, :string
-    add_column :Words, :image, :string
+    add_column :courses, :image, :string
+    add_column :words, :image, :string
   end
 end
